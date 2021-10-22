db7adb0481bfcc46f9b9087ce7a75d72de86b610
maystrova <maystrovakate@gmail.com>
2021-10-22 18:32:10 +0300

    Footer component

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { StyledFooter } from './style'
+
+interface FooterProps {}
+
+const Footer = ({}: FooterProps) => {
+    return <StyledFooter></StyledFooter>
+}
+
+export { Footer }
---
+++ src/Components/Footer/style.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const StyledFooter = styled.div`
+    height: 322px;
+    background-color: rgba(56, 56, 56, 1);
+`
+
+export { StyledFooter }
