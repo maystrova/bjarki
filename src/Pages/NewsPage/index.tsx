77998dbec1a977484d937eb5ab273c36c2aabf14
maystrova <maystrovakate@gmail.com>
2021-11-27 17:57:26 +0300

    NewsPage styles

--- src/Pages/NewsPage/index.tsx
+++ src/Pages/NewsPage/index.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 import { Header, HEADER_TYPE } from 'Components/Header'
 
-import { StyledNewsPage, StyledNewsPageContainer } from './style'
+import {
+    StyledNewsPage,
+    StyledNewsPageContainer,
+    StyledNewsPageImage,
+    StyledNewsPageNews,
+    StyledNewsPageTitle,
+} from './style'
 
 import marocco from './pics/morocco.png'
 import palms from './pics/palms.png'
@@ -13,95 +19,113 @@ interface NewsPageProps {
 
 const NewsPage = ({ onLogInClicked }: NewsPageProps) => {
     return (
-        <StyledNewsPage>
+        <div>
             <Header
                 onLogInClicked={onLogInClicked}
                 headerType={HEADER_TYPE.BLACK}
             />
-            <StyledNewsPageContainer>
-                <div>
-                    <span>News</span>
-                    <span>•</span>
-                    <span>July 9, 2019</span>
-                </div>
-                <h1>My Travel Experiences in Southern Morocco</h1>
-                <p>
-                    In case you're searching for the world's best excursion
-                    goal, Morocco is a top contender. Rough gullies, and
-                    medieval medinas are just piece of what makes this 10-day
-                    Morocco agenda a memorable outing.
-                </p>
-                <p>
-                    Go through the initial two days becoming mixed up in the
-                    labyrinth of rear entryways of the Marrakech medina. Slender
-                    lanes are fixed with unlimited columns of stallholders
-                    selling shoes, lights and material. Markets are stacked high
-                    with natural product, nuts and flavors. Furthermore, mystery
-                    laneways lead to laborers beating cowhide, smiths producing
-                    metal and dyers recoloring fabric, follow days 1 and 2 of
-                    our 3-day Marrakech agenda.
-                </p>
-                <img src={marocco} alt='img' />
-                <div>Toubkal Mountain, High Atlas, Morocco</div>
-                <p>
-                    Mosques, tombs and royal residences show Moroccan building
-                    flare with mosaic tiles, plaster plasterwork and decorated
-                    cedar roofs. In two days you can investigate the best of the
-                    souks and fondouks, the magnificent Moorish design and the
-                    sheer vitality of life in the city.
-                </p>
-                <h2>The Start</h2>
-                <p>
-                    Eleven years ago our founders, Brian and Joe, dropping down
-                    the opposite side you could take the quick course to Aït Ben
-                    Haddou. Be that as it may, that would be frenzy. The
-                    Moroccans have as of late cleared the undeniably
-                    progressively beautiful and intriguing P1506. So transform
-                    left and head into the valley that was before the principle
-                    train course from Marrakech to Timbuktu. Just outside the
-                    Kasbah, Lion D'Or does a shockingly decent lunch.
-                </p>
-                <div>
+            <StyledNewsPage>
+                <div>User</div>
+                <StyledNewsPageContainer>
+                    <div>
+                        <StyledNewsPageNews>
+                            <span>News</span>
+                            <span> • </span>
+                            <span>July 9, 2019</span>
+                        </StyledNewsPageNews>
+                        <StyledNewsPageTitle>
+                            My Travel Experiences in
+                            <br /> Southern Morocco
+                        </StyledNewsPageTitle>
+                    </div>
+
+                    <div>
+                        <p>
+                            In case you're searching for the world's best
+                            excursion goal, Morocco is a top contender. Rough
+                            gullies, and medieval medinas are just piece of what
+                            makes this 10-day Morocco agenda a memorable outing.
+                        </p>
+                        <p>
+                            Go through the initial two days becoming mixed up in
+                            the labyrinth of rear entryways of the Marrakech
+                            medina. Slender lanes are fixed with unlimited
+                            columns of stallholders selling shoes, lights and
+                            material. Markets are stacked high with natural
+                            product, nuts and flavors. Furthermore, mystery
+                            laneways lead to laborers beating cowhide, smiths
+                            producing metal and dyers recoloring fabric, follow
+                            days 1 and 2 of our 3-day Marrakech agenda.
+                        </p>
+                    </div>
+                    <StyledNewsPageImage src={marocco} alt='img' />
+                    <div>Toubkal Mountain, High Atlas, Morocco</div>
                     <p>
-                        Passing through the Atlas Mountains is a delight in
-                        itself and the view today on the drive to Aït Ben Haddou
-                        is momentous. Leave Ouirgane and head back to Tazhnaout,
-                        take a right hand turn onto the P2010 and – when you get
-                        to Aït Ourir – turn right again onto the N9. Passing
-                        through the Atlas Mountains is a delight in itself and
-                        the view today on the drive to Aït Ben Haddou is
-                        momentous. Leave Ouirgane and head back to Tazhnaout,
-                        take a right hand turn onto the P2010.
+                        Mosques, tombs and royal residences show Moroccan
+                        building flare with mosaic tiles, plaster plasterwork
+                        and decorated cedar roofs. In two days you can
+                        investigate the best of the souks and fondouks, the
+                        magnificent Moorish design and the sheer vitality of
+                        life in the city.
                     </p>
-                </div>
-                <div>- Anderson Stuart, Chief Director of Community</div>
-                <img src={palms} alt='img' />
-                <p>
-                    Proceed towards Aït Ben Haddou. On this piece of the drive,
-                    the Ounila stream meets the street and cuts glorious ravines
-                    in the red stone. The base of which are punctuated with
-                    delightful green desert spring. There are a lot of
-                    incredible spots to stop by the roadside, investigate and
-                    take photographs. At last, expect to get to Aït Ben Haddou
-                    for nightfall. Based on a slope, this braced town (ksar)
-                    sits grandly over the banks of the Ounila River. It's
-                    castellated red mud dividers and emotional area summon
-                    pictures of scriptural towns, tragically deceased towns, and
-                    doing combating armed forces. Hollywood has likewise paid
-                    heed. For a considerable length of time the film studios
-                    have made the journey here to film such works of art as
-                    Lawrence of Arabia, Gladiator and Jesus of Nazareth. Next,
-                    take the N10 past Skoura and through level desolate badlands
-                    to the Dades Valley. The R704 exciting bends in the road up
-                    the valley offering great perspectives on red stone gully
-                    and green desert garden. Be that as it may, the feature is
-                    the amazing component of Monkey Fingers. Here little surges
-                    of water have cut rivulets and opening gullies in the stone.
-                    Stop at Isabel bistro, snatch a mint tea and respect the
-                    unusual bulbous stone developments.
-                </p>
-            </StyledNewsPageContainer>
-        </StyledNewsPage>
+                    <h2>The Start</h2>
+                    <p>
+                        Eleven years ago our founders, Brian and Joe, dropping
+                        down the opposite side you could take the quick course
+                        to Aït Ben Haddou. Be that as it may, that would be
+                        frenzy. The Moroccans have as of late cleared the
+                        undeniably progressively beautiful and intriguing P1506.
+                        So transform left and head into the valley that was
+                        before the principle train course from Marrakech to
+                        Timbuktu. Just outside the Kasbah, Lion D'Or does a
+                        shockingly decent lunch.
+                    </p>
+                    <div>
+                        <p>
+                            Passing through the Atlas Mountains is a delight in
+                            itself and the view today on the drive to Aït Ben
+                            Haddou is momentous. Leave Ouirgane and head back to
+                            Tazhnaout, take a right hand turn onto the P2010 and
+                            – when you get to Aït Ourir – turn right again onto
+                            the N9. Passing through the Atlas Mountains is a
+                            delight in itself and the view today on the drive to
+                            Aït Ben Haddou is momentous. Leave Ouirgane and head
+                            back to Tazhnaout, take a right hand turn onto the
+                            P2010.
+                        </p>
+                    </div>
+                    <div>- Anderson Stuart, Chief Director of Community</div>
+                    <StyledNewsPageImage src={palms} alt='img' />
+                    <p>
+                        Proceed towards Aït Ben Haddou. On this piece of the
+                        drive, the Ounila stream meets the street and cuts
+                        glorious ravines in the red stone. The base of which are
+                        punctuated with delightful green desert spring. There
+                        are a lot of incredible spots to stop by the roadside,
+                        investigate and take photographs. At last, expect to get
+                        to Aït Ben Haddou for nightfall. Based on a slope, this
+                        braced town (ksar) sits grandly over the banks of the
+                        Ounila River. It's castellated red mud dividers and
+                        emotional area summon pictures of scriptural towns,
+                        tragically deceased towns, and doing combating armed
+                        forces. Hollywood has likewise paid heed. For a
+                        considerable length of time the film studios have made
+                        the journey here to film such works of art as Lawrence
+                        of Arabia, Gladiator and Jesus of Nazareth. Next, take
+                        the N10 past Skoura and through level desolate badlands
+                        to the Dades Valley. The R704 exciting bends in the road
+                        up the valley offering great perspectives on red stone
+                        gully and green desert garden. Be that as it may, the
+                        feature is the amazing component of Monkey Fingers. Here
+                        little surges of water have cut rivulets and opening
+                        gullies in the stone. Stop at Isabel bistro, snatch a
+                        mint tea and respect the unusual bulbous stone
+                        developments.
+                    </p>
+                </StyledNewsPageContainer>
+                <div>Share</div>
+            </StyledNewsPage>
+        </div>
     )
 }
 
--- src/Pages/NewsPage/style.tsx
+++ src/Pages/NewsPage/style.tsx
@@ -2,10 +2,43 @@ import styled from 'styled-components'
 
 const StyledNewsPage = styled.div`
     padding: 100px 85px;
+    display: flex;
+    justify-content: space-between;
 `
 
 const StyledNewsPageContainer = styled.div`
-    max-width: 1200px;
+    max-width: 750px;
+    display: grid;
+    grid-row-gap: 50px;
 `
 
-export { StyledNewsPage, StyledNewsPageContainer }
+const StyledNewsPageTitle = styled.h1`
+    font-size: 40px;
+    line-height: 48px;
+    font-weight: 500;
+`
+
+const StyledNewsPageNews = styled.div`
+    font-size: 14px;
+    width: 122px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 12px;
+
+    & :first-child {
+        color: rgba(252, 69, 0, 1);
+    }
+`
+
+const StyledNewsPageImage = styled.img`
+    width: 100%;
+`
+
+export {
+    StyledNewsPage,
+    StyledNewsPageContainer,
+    StyledNewsPageNews,
+    StyledNewsPageTitle,
+    StyledNewsPageImage,
+}
